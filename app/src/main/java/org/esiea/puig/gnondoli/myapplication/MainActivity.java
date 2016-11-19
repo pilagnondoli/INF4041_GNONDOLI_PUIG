3f13cc7a722d4232dd793a4bcaa772be38fda230
Your Name <you@example.com>
2016-11-19 15:56:51 +0100

    commit

--- app/src/main/java/org/esiea/puig/gnondoli/myapplication/MainActivity.java
+++ app/src/main/java/org/esiea/puig/gnondoli/myapplication/MainActivity.java
@@ -1,14 +1,27 @@
 package org.esiea.puig.gnondoli.myapplication;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
@@ -40,4 +53,46 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    public void checkAndRequestPermissions() {
+
+
+        Dexter.checkPermissions(new MultiplePermissionsListener() {
+            @Override public void onPermissionsChecked(MultiplePermissionsReport report) {
+                List<String> grantedPermissions = new ArrayList<String>();
+                for(PermissionGrantedResponse response: report.getGrantedPermissionResponses()){
+                    if(!grantedPermissions.contains(response.getPermissionName())){
+                        grantedPermissions.add(response.getPermissionName());
+                    }
+                }
+                Toast.makeText(getApplicationContext(), "Granted permissions:"+grantedPermissions.toString(), Toast.LENGTH_LONG).show();
+            }
+            @Override public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                token.continuePermissionRequest();
+            }
+        }, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.INTERNET);
+
+
+    }
+
+
+
+
+    @Override public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) { //The request code you passed along with the request.
+            //grantResults holds a list of all the results for the permissions requested.
+            for (int grantResult : grantResults) {
+                if (grantResult == PackageManager.PERMISSION_DENIED) {
+                    Log.d("PermissionResult=>", "Denied");
+                    return;
+                }
+            }
+            Log.d("PermissionResult=>", "All Permissions Granted");
+        }
+    }
+
+
+
+
+
 }
